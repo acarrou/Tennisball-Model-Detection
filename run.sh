a72ce5d0642a25112faf0ba23ea2b0a1d816e806
Adrien Carrou <dridriswim@gmail.com>
2022-08-02 10:15:29 -0700

    adds better run and better trained model

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 echo "Running Tennis Ball Detection Script"
-python detect.py --source 0 --weights runs/train/exp/weights/best.pt
+echo "Which project would you like to run? (In the runs/train folder) ex. exp, exp1, exp2, etc."
+read project
+python detect.py --name ${project} --source 0 --weights runs/train/${project}/weights/best.pt
--- train.sh
+++ train.sh
@@ -1,4 +1,6 @@
 echo "Starting the training script"
 echo "How many epochs do you want to train for? (Training Iterations)"
 read epochs
-python train.py --data tennisball.yaml  --device 0 --epochs ${epochs} --weights yolov5s.pt --img 640
+echo "What would you like to name the project? (This will be the name of the folder in runs/train)"
+read project_name
+python train.py --name ${project_name} --data tennisball.yaml  --device 0 --epochs ${epochs} --weights yolov5s.pt --img 640
